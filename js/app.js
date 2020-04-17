fa95bd443c451cf57e73a242e05ae7b54bb93966
Stephen Choe <stphnchoe@gmail.com>
2020-04-17 16:30:14 -0700

    Adds home-display

--- js/app.js
+++ js/app.js
@@ -5,4 +5,23 @@ $(document).ready(function () {
     $(".navbar-burger").toggleClass("is-active");
     $(".navbar-menu").toggleClass("is-active");
   });
+
+  let currImage =
+    "url(https://s3-media0.fl.yelpcdn.com/bphoto/U4YTRl3qadSxlP7YINfxeg/o.jpg)";
+  let nextImage =
+    "url(https://s3-media0.fl.yelpcdn.com/bphoto/qr0yMJraW7lJwUNta00rdQ/o.jpg)";
+  let temp = null;
+
+  const cycleBackgroundImage = () => {
+    let temp = currImage;
+    $(".landing-image").css({
+      "background-image": nextImage,
+    });
+    currImage = nextImage;
+    nextImage = temp;
+  };
+
+  setInterval(function () {
+    cycleBackgroundImage();
+  }, 3000);
 });
